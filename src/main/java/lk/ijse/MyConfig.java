bbac8828538d6d16fc32c3d628704940f0aceab4
thisaru <amithawerahena@gmail.com>
2024-08-04 08:25:20 +0530

    first commit

---
+++ src/main/java/lk/ijse/MyConfig.java
@@ -0,0 +1,14 @@
+package lk.ijse;
+
+import org.springframework.stereotype.Component;
+
+//@Component  // spring ta kiyana widiya
+public class MyConfig {
+
+    public MyConfig(){
+        System.out.println("Hello MyConfig");
+    }
+
+
+
+}
