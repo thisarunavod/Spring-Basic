07daa7804a16826ddc38a2fdeaeec8dbb55354a9
thisaru <amithawerahena@gmail.com>
2024-08-04 11:30:12 +0530

    5th commit

--- src/main/java/lk/ijse/beans/Item.java
+++ src/main/java/lk/ijse/beans/Item.java
@@ -2,7 +2,7 @@ package lk.ijse.beans;
 
 import org.springframework.stereotype.Component;
 
-@Component  /*meka damme natnnm config class ekata ganne na */
+/*@Component*/  /*meka damme natnnm config class ekata ganne na */
 public class Item {
     public Item(){
         System.out.println("Hello Item");
--- src/main/java/lk/ijse/config/Config.java
+++ src/main/java/lk/ijse/config/Config.java
@@ -1,11 +1,17 @@
 package lk.ijse.config;
 
+import lk.ijse.beans.Order;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse") /*package eka dunnama eke sub package wala ewath scan karanawa  */
+@ComponentScan(basePackages = "lk.ijse") /*package eka dunnama eka saha sub package wala ewath scan karanawa  */
 public class Config { /* meya karanne  spring context ekata watenna kamathi aya hoyala denawa */
 
-    public Config() { }
+    @Bean    /* source code ekak nati class ekak bean ekak widiyata register karanna oon awenawaa */
+    public Order order(){
+        return new Order();
+    }
+
 }
